f3f20138e79711dc484aea35714d3a65cd2b3e14
Jarmo Isotalo <jamo@isotalo.fi>
2013-11-04 22:42:19 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TmcEventLogger::Application.config.secret_key_base = 'ff0ffd08fc0b2e2d8f028b8ab7a8306da6b25e7a19a400b7c67f1beceb3e2a251ebb27aa475865e6db0ffa0e73715660818039c20eb88935026080a2d80f3bcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TmcEventLogger::Application.config.session_store :cookie_store, key: '_tmc-event-logger_session'
